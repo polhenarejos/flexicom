5079c26b8d00bee2f13f339abca250720b1f37ce
Pol Henarejos <pol.henarejos@cttc.es>
2012-05-30 10:37:52 +0000

    Added compatibility for packed structures
    git-svn-id: http://repository.cttc.es/svn/SDR/FlexiCom/trunk@4 f0b3029d-c460-4fc5-a45a-8aa6a25956dd

---
+++ include/compat.h
@@ -0,0 +1,10 @@
+#ifndef _INC_COMPAT_H_
+#define _INC_COMPAT_H_
+
+#ifdef _WIN
+#define PACK( __Declaration__ ) __pragma( pack(push, 1) ) __Declaration__ __pragma( pack(pop) )
+#else
+#define PACK( __Declaration__ ) __Declaration__ __attribute__((__packed__))
+#endif
+
+#endif //_INC_COMPAT_H_
