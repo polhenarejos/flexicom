df539ee01406c5e08dfc7d19364d13a5065c03a2
Pol Henarejos <pol.henarejos@cttc.es>
2012-07-12 14:04:56 +0000

    Added frame_conversor. It detects if the input is complex or float.
    git-svn-id: http://repository.cttc.es/svn/SDR/FlexiCom/trunk@67 f0b3029d-c460-4fc5-a45a-8aa6a25956dd

---
+++ tools/frame_conversor.cc
@@ -0,0 +1,42 @@
+// $Id: frame_conversor.cc 433 2010-06-22 07:40:45Z phenarejos $
+
+#include <iostream>
+#include <stdio.h>
+#include <complex>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout<<"USE "<<argv[0]<<" FILE"<<std::endl;
+		exit(-1);
+	}
+	char buf[1024], *real, *imag;
+	//int i = 0;
+	sprintf(buf, "%s.dat", argv[1]);
+	FILE *fp = fopen(argv[1], "r"), *fw = fopen(buf, "w");
+	while (fgets(buf, 1024, fp))
+	{
+		//std::cout<<"Line: "<<++i<<std::endl;
+		buf[strlen(buf)-1]=0;
+		real = buf;
+		while (*real==' ')real++;
+		if ((imag = strchr(real,' ')))
+		{
+			*imag++=0;
+			while (*imag==' ')imag++;
+			std::complex<float> comp(atof(real),atof(imag));
+			fwrite(&comp, sizeof(std::complex<float>), 1, fw);
+		}
+		else
+		{
+			float comp = atof(real);
+			fwrite(&comp, sizeof(float), 1, fw);
+		}
+	}
+	fclose(fw);
+	fclose(fp);
+	return 0;
+}
