de016d63fffe9b8cbe87999d26ac9f46de7847ea
Pol Henarejos <pol.henarejos@cttc.es>
2012-08-23 12:40:00 +0000

    Again another fix
    git-svn-id: http://repository.cttc.es/svn/SDR/FlexiCom/trunk@189 f0b3029d-c460-4fc5-a45a-8aa6a25956dd

--- src/layouts/VLC/Interleaver.cc
+++ src/layouts/VLC/Interleaver.cc
@@ -12,14 +12,9 @@ Interleaver::Interleaver(unsigned int _GF, unsigned int _N, unsigned int _K, uns
 	unsigned int D = ceil(((double)S/N));
 	unsigned int S_block = N * D;
 	unsigned int p = N - (S%N);
-	ivector.resize(S_block);
+	//ivector.resize(S_block);
 	for (unsigned int i = 0; i < S_block; i++)
-	{
-		if (mode == INTERLEAVING)
-			ivector.push_back((i%D)*N + i/D);
-		else
-			ivector[(i%D)*N + i/D] = i;
-	}
+		ivector.push_back((i%D)*N + i/D);
 	set_output_multiple(rs_length);
 }
 Interleaver::sptr Interleaver::Create(unsigned int _GF, unsigned int _N, unsigned int _K, unsigned int _raw_length, unsigned int _rs_length, Mode _mode)
@@ -32,11 +27,20 @@ int Interleaver::work(int no, gr_vector_const_void_star &_i, gr_vector_void_star
 {
 	const int *iptr = (const int *)_i[0];
 	int *optr = (int *)_o[0];
+	
 	for (int n = 0; n < no/rs_length; n++)
 	{
-		for (int i = 0; i < rs_length; i++)
-			*optr++ = *(iptr+ivector[i]);
-		iptr += rs_length;
+		for (int i = 0; i < ivector.size(); i++)
+		{
+			if (mode == INTERLEAVING)
+				*optr++ = *(iptr+ivector[i]);
+			else if (mode == DEINTERLEAVING && ivector[i] < rs_length)
+				*(optr+ivector[i]) = *iptr++;
+		}
+		if (mode == INTERLEAVING)
+			iptr += rs_length;
+		else
+			optr += rs_length;
 	}
 	return no;
 }
