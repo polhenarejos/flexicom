b3835b86fd441d5e8f2cc316d643c2f067a53688
Pol Henarejos <pol.henarejos@cttc.es>
2012-08-27 14:08:49 +0000

    Updating CRC staff
    git-svn-id: http://repository.cttc.es/svn/SDR/FlexiCom/trunk@203 f0b3029d-c460-4fc5-a45a-8aa6a25956dd

--- src/layouts/VLC/bb_Header_cp.cc
+++ src/layouts/VLC/bb_Header_cp.cc
@@ -36,8 +36,8 @@ int bb_Header_cp::general_work(int noutput_items, gr_vector_int &ninput_items, g
 	const int *iptr = (const int *)input_items[0];
 	unsigned char *optr = (unsigned char *)output_items[0];
 	int blocks_to_process = (noutput_items/olen);
-	int *tmp = new int[length+16];
-	memset(tmp,0,sizeof(int)*length+16);
+	int *tmp = new int[length+LayoutVLC::CRC_LENGTH];
+	memset(tmp,0,sizeof(int)*length+LayoutVLC::CRC_LENGTH);
 	int rtd = 0;
 	for (int i = 0; i < blocks_to_process; i++)
 	{
@@ -45,7 +45,7 @@ int bb_Header_cp::general_work(int noutput_items, gr_vector_int &ninput_items, g
 		mtx.lock();
 		if (LayoutVLC::CheckCRC(tmp, length)) //crc ok!!
 		{
-			for (int n = 0; n < length-16; n += sizeof(unsigned char)*8)
+			for (int n = 0; n < length-LayoutVLC::CRC_LENGTH; n += sizeof(unsigned char)*8)
 			{
 				*optr++ = (unsigned char)LayoutVLC::bi2dec((int *)iptr+n, sizeof(unsigned char)*8);
 				rtd++;
