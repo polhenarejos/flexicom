5230fe203392af9762d1fa78e1325f6d0c0026c2
Pol Henarejos <pol.henarejos@cttc.es>
2012-08-30 14:04:57 +0000

    Ups, timing
    git-svn-id: http://repository.cttc.es/svn/SDR/FlexiCom/trunk@222 f0b3029d-c460-4fc5-a45a-8aa6a25956dd

---
+++ src/layouts/VLC/Timing.cc
@@ -0,0 +1,47 @@
+// $Id$
+
+#include "Timing.h"
+#include <gr_io_signature.h>
+#include <volk/volk.h>
+
+Timing::Timing(unsigned int _decim, unsigned int _syms) :
+	gr_sync_decimator("Timing", gr_make_io_signature(1, 1, sizeof(float)), gr_make_io_signature(1, 1, sizeof(int)), _decim),
+	syms(_syms)
+{
+	set_output_multiple(syms);
+}
+Timing::sptr Timing::Create(unsigned int _decim, unsigned int _syms)
+{
+	return sptr(new Timing(_decim, _syms));
+}
+int Timing::work(int no, gr_vector_const_void_star &_i, gr_vector_void_star &_o) 
+{
+	const float *iptr = (const float *)_i[0];
+	int *optr = (int *)_o[0];
+	unsigned int decim = decimation();
+	float *E = (float *)_aligned_malloc(sizeof(float)*no*decim, 16);
+	if (is_unaligned())
+		volk_32f_x2_multiply_32f_u(E, iptr, iptr, no);
+	else
+		volk_32f_x2_multiply_32f_a(E, iptr, iptr, no);
+	for (int n = 0; n < no; n += syms)
+	{
+		float mx = 0;
+		int idx = 0;
+		for (int i = 0; i < decim; i++)
+		{
+			float e = 0;
+			for (int j = 0; j < syms; j++)
+				e += E[i*decim+j];
+			if (e > mx)
+			{
+				mx = e;
+				idx = i;
+			}
+		}
+		for (int i = 0; i < syms; i++)
+			*optr++ = iptr[i*decim+idx] > 0 ? 1 : 0;
+		iptr += syms*decim;
+	}
+	return no;
+}
---
+++ src/layouts/VLC/Timing.h
@@ -0,0 +1,32 @@
+// $Id$                      
+                                                                           
+#ifndef _INC_TIMING_H_
+#define _INC_TIMING_H_
+
+#include <gr_sync_decimator.h>
+
+/*! \brief Timing block performs a maximum likelihood timing synchronization operation
+
+*  According to the idea that appears in section 7.5.1 of the book: "Synchronization Techniques for Digital Receivers", Mengali, D'Andrea, 1997, 
+*  in the derivation of the ML detector says that the optimum timing estimate corresponds to the sampling epoch tau that maximizes the energy of the 
+*  sequence {y(iT + tau)}.
+*/
+                                                               
+class Timing : public gr_sync_decimator
+{
+	public:
+		typedef boost::shared_ptr<Timing> sptr;
+	   /**
+       * The creation of the Timing requires 2 parameters: 
+       * @param _symbols:  the number of symbols needed to perform the average and obtain the sampling epoch tau.
+       * @param _decimation: the decimation rate that has to be applied as a consequence of the oversampling factor applied at transmission
+       */
+		static sptr Create(unsigned int, unsigned int = 1);
+		int work(int, gr_vector_const_void_star &, gr_vector_void_star &);
+
+	private:
+		Timing(unsigned int, unsigned int);
+		int syms;
+};                                                                         
+
+#endif //_INC_TIMING_H_
