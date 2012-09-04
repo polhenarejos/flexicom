e8cb270f34dfef7f31adfc6d345f49415055937c
Pol Henarejos <pol.henarejos@cttc.es>
2012-09-04 10:21:00 +0000

    Fix with energy index
    git-svn-id: http://repository.cttc.es/svn/SDR/FlexiCom/trunk@241 f0b3029d-c460-4fc5-a45a-8aa6a25956dd

--- src/layouts/VLC/Timing.cc
+++ src/layouts/VLC/Timing.cc
@@ -22,11 +22,11 @@ int Timing::work(int no, gr_vector_const_void_star &_i, gr_vector_void_star &_o)
 	const float *iptr = (const float *)_i[0];
 	int *optr = (int *)_o[0];
 	unsigned int decim = decimation();
-	float *E = (float *)malloc16Align(sizeof(float)*no*decim);
+	float *E = (float *)malloc16Align(sizeof(float)*no*decim), *Ey = E;
 	if (is_unaligned())
-		volk_32f_x2_multiply_32f_u(E, iptr, iptr, no);
+		volk_32f_x2_multiply_32f_u(E, iptr, iptr, no*decim);
 	else
-		volk_32f_x2_multiply_32f_a(E, iptr, iptr, no);
+		volk_32f_x2_multiply_32f_a(E, iptr, iptr, no*decim);
 	for (int n = 0; n < no; n += syms)
 	{
 		float mx = 0;
@@ -35,7 +35,7 @@ int Timing::work(int no, gr_vector_const_void_star &_i, gr_vector_void_star &_o)
 		{
 			float e = 0;
 			for (int j = 0; j < syms; j++)
-				e += E[i*decim+j];
+				e += Ey[i*decim+j];
 			if (e > mx)
 			{
 				mx = e;
@@ -45,6 +45,7 @@ int Timing::work(int no, gr_vector_const_void_star &_i, gr_vector_void_star &_o)
 		for (int i = 0; i < syms; i++)
 			*optr++ = iptr[i*decim+idx] > 0 ? 1 : 0;
 		iptr += syms*decim;
+		Ey += syms*decim;
 	}
 	free16Align(E);
 	return no;
