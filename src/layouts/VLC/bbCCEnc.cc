c2684b2e0c7b6cfdab8d96dfd33b0c95a1b2efa6
Pol Henarejos <pol.henarejos@cttc.es>
2019-09-23 15:28:28 +0200

    Adding header expansion and license GPL text

--- src/layouts/VLC/bbCCEnc.cc
+++ src/layouts/VLC/bbCCEnc.cc
@@ -1,4 +1,22 @@
-// $Id$
+/* 
+ * This file is part of the FlexiCom distribution (https://github.com/polhenarejos/flexicom).
+ * Copyright (c) 2012-2020 Pol Henarejos, at Centre Tecnologic de Telecomunicacions de Catalunya (CTTC).
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+/* $Id$ */
+/* $Format:%ci$ ($Format:%h$) by $Format:%an%$ */
+
 #include "bbCCEnc.h"
 #include <gnuradio/io_signature.h>
 #include "vlc_convolutional_coding.h"
